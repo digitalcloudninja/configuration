06a87e793fcd6ccaa345245371e3630791660592
DigitalCloud Ninja <admin@digitalcloud.ninja>
2024-06-28 22:17:18 -0600

    Create configuration-service application

--- src/main/java/ninja/digitalcloud/cloud/configuration/Main.java
+++ src/main/java/ninja/digitalcloud/cloud/configuration/Main.java
@@ -1,8 +1,12 @@
-package ninja.digitalcloud.cloud.starter;
+package ninja.digitalcloud.cloud.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Main {
 
